fb55af4e988a4fb626bd4ca2b036645ee6df6384
Osaid-Basha <albashaosayd@gmail.com>
2025-09-10 23:59:49 +0300

    Update role.php

--- app/Models/role.php
+++ app/Models/role.php
@@ -4,15 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Role extends Model
+class role extends Model
 {
+    //
     protected $fillable = [
+
         'id',
         'type',
-    ];
 
-    public function users()
-    {
-        return $this->hasMany(User::class);
-    }
+
+    ];
+  public function users()
+{
+    return $this->hasMany(User::class);
+}
+
 }
